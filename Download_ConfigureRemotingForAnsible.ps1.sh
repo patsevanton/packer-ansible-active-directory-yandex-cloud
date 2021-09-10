9125cd6e449c7875ab82ae556318e01deea63c36
Anton Patsev <patsev.anton@gmail.com>
2021-09-10 18:40:50 +0600

    Download ConfigureRemotingForAnsible.ps1 as file

--- Download_ConfigureRemotingForAnsible.ps1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! command -v curl &> /dev/null
-then
-    echo "curl could not be found"
-    exit
-fi
-
-curl https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1 --output ConfigureRemotingForAnsible.ps1
